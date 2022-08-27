e7fd91a85e5f4f991ff6ce4ced9d53f638ca789e
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-27 17:09:51 +0630

    Updating VideoCard

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -5,6 +5,7 @@ export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 const options = {
   params: {
     maxResults: 50,
+    order: "date",
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
