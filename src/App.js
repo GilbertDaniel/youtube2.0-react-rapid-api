d333af1f8af82ccd780d6eacfc127e7238773ae0
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-27 14:59:49 +0630

    Implementing Components and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from "./components";
 
-const App = () => {
-  return (
-    <h2>APP</h2>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
